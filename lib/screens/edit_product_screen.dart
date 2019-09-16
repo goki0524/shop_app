847ad542c121fe15b7e3dc4c1d1c21f6e7670aa6
goki sakaguchi <goki0524@gmail.com>
2019-09-16 16:26:57 +0900

    finish 218

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -8,6 +8,8 @@ class EditProductScreen extends StatefulWidget {
 }
 
 class _EditProductScreenState extends State<EditProductScreen> {
+final _priceFocusNode = FocusNode();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,11 +19,23 @@ class _EditProductScreenState extends State<EditProductScreen> {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Form(
+          // たくさんのフォームを縦に並べる場合,ListViewだと子ウィジェットを動的に削除・追加するため入力データが消失する場合がある.
+          // フォームを縦に配置する場合は,SingleChildScrollView + Columnを使用したほうが良い.(入力データは失われない)
           child: ListView(
             children: <Widget>[
               TextFormField(
                 decoration: InputDecoration(labelText: 'Title'),
                 textInputAction: TextInputAction.next,
+                onFieldSubmitted: (_) {
+                  // キーボードの次へを押すとfocusNodeが指定されているフォームに飛ぶ
+                  FocusScope.of(context).requestFocus(_priceFocusNode);
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Price'),
+                textInputAction: TextInputAction.next,
+                keyboardType: TextInputType.number,
+                focusNode: _priceFocusNode,
               ),
             ],
           ),
