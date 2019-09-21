c826bfb6a31554217958f0bee55591b5f2bceeb6
goki sakaguchi <goki0524@gmail.com>
2019-09-21 19:18:55 +0900

    finish 250

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -5,8 +5,32 @@ import '../providers/orders.dart' show Orders;
 import '../widgets/order_item.dart';
 import '../widgets/app_drawer.dart';
 
-class OrdersScreen extends StatelessWidget {
+class OrdersScreen extends StatefulWidget {
   static const routeName = '/orders';
+
+  @override
+  _OrdersScreenState createState() => _OrdersScreenState();
+}
+
+class _OrdersScreenState extends State<OrdersScreen> {
+  var _isLoading = false;
+
+  @override
+  void initState() {
+    // ページを読み出した時に,fetchする.
+    // initStateではcontextを取得できないため,ゼロ遅延処理(Future.delayed(Duration.zero))をしている
+    // 無駄にビルドさせないために,listen: false
+    Future.delayed(Duration.zero).then((_) async {
+      setState(() {
+        _isLoading = true;
+      });
+      await Provider.of<Orders>(context, listen: false).fetchAndSetOrders();
+      setState(() {
+        _isLoading = false;
+      });
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,12 +41,16 @@ class OrdersScreen extends StatelessWidget {
         title: Text('注文履歴'),
       ),
       drawer: AppDrawer(),
-      body: ListView.builder(
-        itemCount: orderData.orders.length,
-        itemBuilder: (ctx, i) => OrderItem(
-          orderData.orders[i],
-        ),
-      ),
+      body: _isLoading
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : ListView.builder(
+              itemCount: orderData.orders.length,
+              itemBuilder: (ctx, i) => OrderItem(
+                orderData.orders[i],
+              ),
+            ),
     );
   }
 }
